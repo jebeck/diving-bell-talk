0b1f4d03f98541ce80d402e53fac20f899ac9e61
Jana E. Beck <jana.eliz.beck@gmail.com>
2017-08-13 01:32:13 -0700

    thesis done ✅, start on JDB intro

--- presentation/index.js
+++ presentation/index.js
@@ -1,9 +1,20 @@
 // Import React
 import React from "react";
+import shuffle from "shuffle-array";
 
 // Import Spectacle Core tags
-import { Deck, Heading, Link, Slide } from "spectacle";
-import { FaGithub, FaTwitter } from "react-icons/lib/fa";
+import {
+  Appear,
+  BlockQuote,
+  Cite,
+  Deck,
+  Heading,
+  Link,
+  Quote,
+  S,
+  Slide
+} from "spectacle";
+import { FaExternalLink, FaGithub, FaTwitter } from "react-icons/lib/fa";
 
 // Import image preloader util
 import preloader from "spectacle/lib/utils/preloader";
@@ -15,12 +26,21 @@ import createTheme from "spectacle/lib/themes/default";
 require("normalize.css");
 require("spectacle/lib/themes/default/index.css");
 
-const images = {};
+const images = {
+  brixtonTreadmill: require("../assets/images/treadmill-Brixton-prison.jpg"),
+  empathy: require("../assets/images/empathy.jpg"),
+  empathyShoes: require("../assets/images/empathy-shoes.jpg"),
+  okGoTreadmill: require("../assets/images/ok-go-treadmill.gif"),
+  oscarWilde: require("../assets/images/oscar-wilde.jpg"),
+  prestonTreadmill: require("../assets/images/treadmill-Preston-prison.jpg"),
+  treadmill: require("../assets/images/treadmill.jpg"),
+  williamCubitt: require("../assets/images/william-cubitt.jpg")
+};
 
 preloader(images);
 
 const themeColors = {
-  primary: "white",
+  primary: "snow",
   secondary: "#291829",
   tertiary: "#323E6A",
   quartenary: "#E29189"
@@ -56,44 +76,270 @@ export default class Presentation extends React.Component {
           >
             http://janabeck.com/diving-bell-talk/
           </Link>
-        </Slide>
-        <Slide>
           <Heading
             lineHeight={1.35}
-            size={2}
+            size={3}
             textAlign="left"
             textColor="tertiary"
           >
             <Link
               href="https://github.com/jebeck"
               target="_blank"
-              textColor="quartenary"
+              textColor="tertiary"
             >
-              <FaGithub />
+              <span style={{ color: themeColors.quartenary }}>
+                <FaGithub />+:&nbsp;
+              </span>@jebeck
             </Link>
-            <span style={{ color: themeColors.quartenary }}>
-              +:
-            </span>&nbsp;@jebeck
           </Heading>
           <Heading
             lineHeight={1.35}
-            size={2}
+            size={3}
             textAlign="left"
             textColor="tertiary"
           >
             <Link
               href="https://twitter.com/iPancreas"
               target="_blank"
-              textColor="quartenary"
+              textColor="tertiary"
             >
-              <FaTwitter />
+              <span style={{ color: themeColors.quartenary }}>
+                <FaTwitter />:&nbsp;
+              </span>@iPancreas
             </Link>
-            <span style={{ color: themeColors.quartenary }}>
-              :
-            </span>&nbsp;@iPancreas
           </Heading>
         </Slide>
-        {/* TODO: company slide? */}
+        <Slide>
+          <Heading lineHeight={1.5} size={2}>
+            dataviz eng
+          </Heading>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="488.033 281.743 120.333 120.307"
+            width="45vw"
+            height="45vh"
+          >
+            <path
+              fill="#847C78"
+              d="M607.707 340.293l-13.164-13.164 11.39-11.39c.878-.88.878-2.303 0-3.182s-2.304-.88-3.183 0l-11.39 11.39-18.373-18.375 11.397-11.397c.88-.88.88-2.303 0-3.182s-2.303-.88-3.182 0l-11.397 11.397-20.015-20.014c-.844-.844-2.338-.844-3.182 0l-19.93 19.93-11.312-11.312c-.88-.88-2.303-.88-3.182 0s-.88 2.303 0 3.182l11.312 11.312-18.39 18.39-11.32-11.32c-.88-.88-2.303-.88-3.182 0s-.88 2.303 0 3.182l11.32 11.32-13.233 13.233c-.42.422-.658.994-.658 1.59s.237 1.17.66 1.592l13.163 13.164-11.388 11.387c-.88.88-.88 2.303 0 3.182.44.438 1.015.658 1.59.658s1.152-.22 1.592-.66l11.387-11.388 18.373 18.373-11.397 11.397c-.88.88-.88 2.303 0 3.182.44.44 1.015.66 1.59.66s1.152-.22 1.592-.66l11.397-11.397 20.016 20.015c.44.44 1.015.66 1.59.66s1.152-.22 1.592-.66l19.93-19.93 11.312 11.312c.44.44 1.015.66 1.59.66s1.152-.22 1.592-.66c.88-.88.88-2.303 0-3.182l-11.312-11.312 18.39-18.39 11.32 11.32c.44.438 1.016.658 1.592.658s1.15-.22 1.59-.66c.88-.878.88-2.302 0-3.18l-11.32-11.32 13.233-13.233c.88-.877.88-2.3 0-3.18zM548.2 287.148l18.424 18.424-18.34 18.34-18.423-18.425 18.34-18.34zm-54.735 54.736l11.643-11.643 11.573 11.574-11.64 11.643-11.575-11.573zm14.755 14.755l13.233-13.235c.88-.88.88-2.303 0-3.182L508.29 327.06l18.39-18.39 18.423 18.423-13.284 13.284c-.88.88-.88 2.303 0 3.182l13.112 13.112-18.34 18.34-18.372-18.373zm40.065-26.365l11.522 11.522-11.693 11.693-11.522-11.522 11.693-11.693zm-.085 66.343l-18.425-18.425 18.34-18.34 18.424 18.426-18.34 18.338zm21.52-21.52l-18.424-18.425 13.284-13.284c.88-.88.88-2.304 0-3.183l-13.113-13.113 18.34-18.34 18.373 18.375-13.232 13.232c-.88.88-.88 2.303 0 3.182l13.164 13.164-18.39 18.39zm21.573-21.573l-11.573-11.573 11.64-11.642 11.574 11.574-11.64 11.64z"
+            />
+          </svg>
+          <Heading fit size={1}>
+            Stitch Fix Algorithms
+          </Heading>
+          <Link
+            href="http://algorithms-tour.stitchfix.com/"
+            target="_blank"
+            textColor="tertiary"
+          >
+            (<FaExternalLink />&nbsp;take the Stitch Fix algo tour!)
+          </Link>
+        </Slide>
+        <Slide bgImage={images.treadmill}>
+          <Appear>
+            <Heading
+              bgColor="rgba(255,255,255,0.6666666667)"
+              lineHeight={1.5}
+              padding="0 0 1rem 0"
+              size={2}
+              textColor="tertiary"
+            >
+              (a treadmill)
+            </Heading>
+          </Appear>
+          <div style={{ height: "100vh", width: "100vw" }}>
+            <Link
+              href="https://www.flickr.com/photos/29638108@N06/5618665304/in/faves-134718242@N08/"
+              target="_blank"
+              textColor="white"
+              textSize="0.5em"
+              style={{ position: "fixed", bottom: "1rem", left: 0 }}
+            >
+              Image by Jennifer C. (CC BY 2.0)
+            </Link>
+          </div>
+        </Slide>
+        <Slide
+          style={{
+            backgroundColor: "#e5d8c6",
+            backgroundImage: `url(${images.brixtonTreadmill})`,
+            backgroundPosition: "center center",
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "contain"
+          }}
+        >
+          <div style={{ height: "100vh", width: "100vw" }}>
+            <Link
+              href="http://www.bbc.co.uk/programmes/articles/4w8bVrKRqQDP4fKl0b8XzdW/the-dark-history-of-the-treadmill"
+              target="_blank"
+              textColor="quartenary"
+              textSize="0.5em"
+              style={{ position: "fixed", bottom: "1rem", left: "-8rem" }}
+            >
+              Image via the BBC
+            </Link>
+          </div>
+        </Slide>
+        <Slide
+          style={{
+            backgroundColor: "#e5d8c6",
+            backgroundImage: `url(${images.williamCubitt})`,
+            backgroundPosition: "center center",
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "contain"
+          }}
+        >
+          <div style={{ height: "100vh", width: "100vw" }}>
+            <Link
+              href="http://www.bbc.co.uk/programmes/articles/4w8bVrKRqQDP4fKl0b8XzdW/the-dark-history-of-the-treadmill"
+              target="_blank"
+              textColor="quartenary"
+              textSize="0.5em"
+              style={{ position: "fixed", bottom: "1rem", left: "-8rem" }}
+            >
+              Image via the BBC
+            </Link>
+          </div>
+        </Slide>
+        <Slide
+          style={{
+            backgroundColor: "#e5d8c6",
+            backgroundImage: `url(${images.brixtonTreadmill})`,
+            backgroundPosition: "center center",
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "contain"
+          }}
+        >
+          <div style={{ height: "100vh", width: "100vw" }}>
+            <Link
+              href="http://www.bbc.co.uk/programmes/articles/4w8bVrKRqQDP4fKl0b8XzdW/the-dark-history-of-the-treadmill"
+              target="_blank"
+              textColor="quartenary"
+              textSize="0.5em"
+              style={{ position: "fixed", bottom: "1rem", left: "-8rem" }}
+            >
+              Image via the BBC
+            </Link>
+          </div>
+        </Slide>
+        <Slide
+          style={{
+            backgroundColor: "#e5d8c6",
+            backgroundImage: `url(${images.prestonTreadmill})`,
+            backgroundPosition: "center center",
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "contain"
+          }}
+        >
+          <div style={{ height: "100vh", width: "100vw" }}>
+            <Link
+              href="https://www.flickr.com/photos/rpsmithbarney/4266533201/in/faves-134718242@N08/"
+              target="_blank"
+              textColor="quartenary"
+              textSize="0.5em"
+              style={{ position: "fixed", bottom: "1rem", left: "0" }}
+            >
+              Public domain image via the Preston Digital Archive
+            </Link>
+          </div>
+        </Slide>
+        {/* TODO: slide of Ben Lomond from Dad's kitchen */}
+        <Slide
+          style={{
+            backgroundColor: "#e5d8c6",
+            backgroundImage: `url(${images.oscarWilde})`,
+            backgroundPosition: "center center",
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "contain"
+          }}
+        >
+          <div style={{ height: "100vh", width: "100vw" }}>
+            <Link
+              href="https://commons.wikimedia.org/w/index.php?curid=5459869"
+              target="_blank"
+              textColor="primary"
+              textSize="0.5em"
+              style={{ position: "fixed", bottom: "1rem", left: "0" }}
+            >
+              Public domain image by Napoleon Sarony via the Wikimedia Commons
+            </Link>
+          </div>
+        </Slide>
+        <Slide bgImage={images.treadmill}>
+          <Heading
+            bgColor="rgba(255,255,255,0.6666666667)"
+            lineHeight={1.5}
+            padding="0 0 1rem 0"
+            size={2}
+            textColor="tertiary"
+          >
+            (a treadmill)
+          </Heading>
+          <div style={{ height: "100vh", width: "100vw" }}>
+            <Link
+              href="https://www.flickr.com/photos/29638108@N06/5618665304/in/faves-134718242@N08/"
+              target="_blank"
+              textColor="white"
+              textSize="0.5em"
+              style={{ position: "fixed", bottom: "1rem", left: 0 }}
+            >
+              Image by Jennifer C. (CC BY 2.0)
+            </Link>
+          </div>
+        </Slide>
+        <Slide bgImage={images.okGoTreadmill} />
+        <Slide>
+          <Heading lineHeight={1.5} fit>
+            the inventor is dead
+          </Heading>
+          <Heading lineHeight={1.5}>💀 😵 💀</Heading>
+          <Heading lineHeight={1.5} fit>
+            <S type="strikethrough">long live the inventor!</S>
+          </Heading>
+        </Slide>
+        <Slide>
+          <Heading fit>technologies have</Heading>
+          <Heading fit>histories</Heading>
+          <Heading>(and contexts)</Heading>
+        </Slide>
+        <Slide>
+          <Heading fit>but histories (and contexts)</Heading>
+          <Heading>
+            are{" "}
+            <S textColor="secondary" type="bold">
+              not
+            </S>
+          </Heading>
+          <Heading fit>definitive</Heading>
+        </Slide>
+        {/* TODO: Jean-Do intro slide(s) */}
+        <Slide bgColor="secondary">
+          <BlockQuote>
+            <Quote textColor="primary">
+              You survive, but you survive with what is so aptly known as
+              “locked-in syndrome.”
+            </Quote>
+            <Cite textColor="tertiary">Jean-Domnique Bauby</Cite>
+          </BlockQuote>
+        </Slide>
+        <Slide bgColor="secondary">
+          <BlockQuote>
+            <Quote textColor="primary">
+              Paralyzed from head to toe, the patient, his mind intact, is
+              imprisoned inside his own body, unable to speak or move.
+            </Quote>
+            <Cite textColor="tertiary">Jean-Domnique Bauby</Cite>
+          </BlockQuote>
+        </Slide>
+        <Slide bgColor="secondary">
+          <BlockQuote>
+            <Quote textColor="primary">
+              In my case, blinking my left eyelid is my only means of
+              communication.
+            </Quote>
+            <Cite textColor="tertiary">Jean-Domnique Bauby</Cite>
+          </BlockQuote>
+        </Slide>
       </Deck>
     );
   }
